c414b62a35ec8f697fc8684e8d3acd448922ac73
Radoslav Cvetkov <radoslavcv9@gmail.com>
2020-08-10 09:22:51 +0200

    First initialization, radora in english and german

---
+++ assets/js/book-store-logic.js
@@ -0,0 +1,144 @@
+// Book Class: Represents a Book
+
+class Book {
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class:  Handle UI Tasks
+class UI {
+
+    static displayBooks(){
+        
+        const books = Store.getBooks();
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book){
+        // Getting the tbody
+        const list = document.querySelector('#book-list');
+
+        // Creating a row for the tbody
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+        // Adding the element 
+        list.appendChild(row);
+    }
+
+    static deleteBook(elementToDelete){
+        if(elementToDelete.classList.contains('delete')) {
+            elementToDelete.parentElement.parentElement.remove();
+            UI.showAlert('Book Removed', 'success');
+        }
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        // Inserting the past in message text in the div
+        div.appendChild(document.createTextNode(message));
+
+        // Grabbing the parent-element (<div class="container mt-4">)
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        // Inserting the div before the form (book-form)
+        container.insertBefore(div,form);
+        // Make vanish 
+        setTimeout(() => 
+            document.querySelector('.alert').remove(), 2500);
+    }
+
+    static clearFields(){
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+}
+
+// Store Class: Handles Storage
+
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books',JSON.stringify(books));       
+    }
+}
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add Book
+document.querySelector('#book-form').addEventListener('submit', (e) =>{
+    // Prevent default action - beacuse of 'submit' - screen/console refreshes
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Values validation
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+
+    }else{
+        // Instantiate Book
+        const book = new Book(title, author, isbn);
+        // Add to UI
+        UI.addBookToList(book);
+        // Add book to store
+        Store.addBook(book);
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+        // Clear fields
+        UI.clearFields();
+    }
+});
+
+// Event: Remove Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // e.target - returns the object to which the event is triggered  -> console.log(e.target);
+    // Remove book from UI and show success message
+    UI.deleteBook(e.target);
+    
+    // Remove book from local storage. e.target is the red X clicked, parent is therefore
+    // the '<<td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>>' and the 
+    // previousElementSibling gives us the '<td>${book.isbn}</td>' which is unique
+    //         
+    //         <td>${book.title}</td>
+    //         <td>${book.author}</td>
+    //         <td>${book.isbn}</td>
+    //         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+});
